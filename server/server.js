86629e8e6a59ff20fece1a48383ab10c56160940
anshoolp-endure <anshoolp.endure@gmail.com>
2025-08-21 17:14:34 +0530

    Add Prisma schema and update server response format

--- server/server.js
+++ server/server.js
@@ -5,7 +5,12 @@ const app = express()
 app.use(express.json())
 
 app.get("/", (req, res) => {
-    res.send(JSON.stringify({ "msg" : "Yoo" }))
+    res.send(JSON.stringify('yoooooooooooooooooooooooooooooo'))
+})
+
+app.post("/post", (req, res) => {
+    const data = req.body;
+    res.status(201).json({"message": "Item created", "Item" : data})
 })
 
 const PORT = 3001;
