5f0fea8a82400583bf578ddcce5b75209a16b59b
Rhythm Naik <rhythmstay4u@gmail.com>
2025-09-01 18:46:55 +0530

    feat: add lucide-react dependency for enhanced icon support

--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -13,8 +13,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body suppressHydrationWarning={true}>
+    <html lang="en" suppressHydrationWarning>
+      <body>
         <ThemeProvider
           attribute="class"
           defaultTheme="system"
