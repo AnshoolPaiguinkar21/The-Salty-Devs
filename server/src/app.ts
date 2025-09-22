8f07411027a7e208f215e1cd2cf10aa3735bf2f3
Rhythm Naik <rhythmstay4u@gmail.com>
2025-09-22 19:21:39 +0530

    Refactor app.ts for consistent formatting and style

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,14 +1,12 @@
-import express from "express"
-import routes from "@routes/index.ts"
-import cookieParser from "cookie-parser"
+import express from 'express';
+import routes from '@routes/index.ts';
+import cookieParser from 'cookie-parser';
 
-const app = express()
+const app = express();
 
-app.use(cookieParser())
-app.use(express.json())
-app.use(express.urlencoded({extended: false}))
-app.use(routes)
+app.use(cookieParser());
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(routes);
 
 export default app;
-
-//hi
